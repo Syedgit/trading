4de381f17bdd896115d7d07c701fae54027bf376
Syed Hussain <shussain@docs.rutgers.edu>
2017-01-12 13:39:10 -0500

    Update development.js

--- server/config/development.js
+++ server/config/development.js
@@ -1,4 +1,4 @@
-Create environemnt and add development.js into that folder fo rlocal host.
+//Create environemnt and add development.js into that folder fo rlocal host.
 
 
 'use strict';
@@ -16,3 +16,62 @@ module.exports = {
   seedDB: false
 
 };
+
+
+//Create index.js under envirnoment add this code into it.
+
+'use strict';
+
+var path = require('path');
+var _ = require('lodash');
+
+function requiredProcessEnv(name) {
+  if (!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable');
+  }
+  return process.env[name];
+}
+
+// All configurations will extend these options
+// ============================================
+var all = {
+  env: process.env.NODE_ENV,
+
+  // Root path of server
+  root: path.normalize(__dirname + '/../../..'),
+
+  // Server port
+  port: process.env.PORT || 9000,
+
+  // Server IP
+  ip: process.env.IP || '0.0.0.0',
+
+  // Should we populate the DB with sample data?
+  seedDB: false,
+
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'angular-modeler-secret'
+  },
+
+  // MongoDB connection options
+  mongo: {
+    options: {
+      db: {
+        safe: true
+      }
+    }
+  }
+};
+
+var envConf = 'development';
+if(['dit', 'lsp', 'prod', 'st', 'uat'].indexOf(process.env.NODE_ENV) >= 0) {
+  envConf = 'production';
+}
+
+// Export the config object based on the NODE_ENV
+// ==============================================
+module.exports = _.merge(
+  all,
+  require('./shared'),
+  require('./' + envConf + '.js') || {});
