e0c251fb185d5ff0dd9d5c9578c9ecfabf2ced34
Syed Hussain <shussain@docs.rutgers.edu>
2017-01-12 12:49:23 -0500

    Update routes.js

---
+++ server/routes.js
@@ -0,0 +1,26 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+import errors from './components/errors';
+import path from 'path';
+
+export default function(app) {
+  // Insert routes below
+  app.use('/api/groups', require('./api/group'));
+  app.use('/api/users', require('./api/user'));
+  app.use('/api/diagrams', require('./api/diagram'));
+  app.post('/fileUpload', require('./api/fileUpload'));
+  app.use('/api/newUsers', require('./api/Access'));
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+   .get(errors[404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get((req, res) => {
+      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+    });
+}
