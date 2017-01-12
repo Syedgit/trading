13784ca4d6008fd1dfe9a64fbcd09a7ef6784be1
Syed Hussain <shussain@docs.rutgers.edu>
2017-01-12 12:48:43 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -47,3 +47,62 @@ console.log('Magic happens on port ' + port);
 
 // expose app           
 exports = module.exports = app;
+
+/*
+/**
+ * Main application file
+ */
+
+'use strict';
+
+import express from 'express';
+import mongoose from 'mongoose';
+mongoose.Promise = require('bluebird');
+import config from './config/environment';
+import http from 'http';
+import https from 'https';
+import * as fs from 'fs';
+import bodyParser  from 'body-parser';
+// Connect to MongoDB
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+});
+
+// Populate databases with sample data
+if (config.seedDB) { require('./config/seed'); }
+
+// Setup server
+var app = express();
+var server = http.createServer(app);
+
+if(process.env.NODE_ENV !== 'development') {
+  var credentials = {
+    key: fs.readFileSync('../keyCert/blink.pem'),
+    cert: fs.readFileSync('../keyCert/blink.pem')
+  };
+  var https_server = https.createServer(credentials, app);
+}
+
+require('./config/express')(app);
+require('./routes')(app);
+
+// Start server
+function startServer() {
+  server.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
+
+  if(process.env.NODE_ENV !== 'development') {
+    https_server.listen(config.https_port, config.ip, function () {
+      console.log('Express server listening on %d, in %s mode', config.https_port, app.get('env'));
+    });
+  }
+}
+
+setImmediate(startServer);
+
+// Expose app
+exports = module.exports = app;
+*/
